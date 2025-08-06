8bfd2e1620a9c868422cd73bde3af602c76219fb
ItsJustTheBeginning474264 <ishanimurkewar2006@gmail.com>
2025-08-06 13:57:16 +0530

    Add PWA Suport

--- script.js
+++ script.js
@@ -264,3 +264,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js")
+      .then(reg => console.log("✅ Service Worker registered"))
+      .catch(err => console.error("❌ SW registration failed", err));
+  });
+}
